22fc070fbdca549cf249707f3b413990cb49b153
Yuki Kudo <yukkytheworld@gmail.com>
2023-04-26 23:58:45 +0900

    mod json

--- app/controllers/api/v1/pauling_controller.rb
+++ app/controllers/api/v1/pauling_controller.rb
@@ -6,16 +6,16 @@ class Api::V1::PaulingController < ApiController
             retJsonText = { 'instrument' => @instrument.serial, 'mode' => @instrument.current_reserve_event.execution_type }
             return render :json => retJsonText
         else
-            return "{\"reservation_exists\": false}"
+            return render :json => {reservation_exists: false}
         end
     end
 
     def complete_resevation
         if @instrument.current_reserve_event.present?
             @instrument.current_reserve_event.complete
-            return "{\"completed\": true}"
+            return render :json => {completed: true}
         else
-            return "{\"reservation_exists\": false}"
+            return render :json => {reservation_exists: false}
         end
     end
 
